d182a9ebc079230b0aac1b52ec316d92e6d000ac
Hugh Wells <wellshb@deanclose.org.uk>
2015-07-27 16:34:29 +0100

    Some stuff...

--- origin_recieve.py
+++ origin_recieve.py
@@ -1,5 +1,5 @@
 import send_text
 def origin( number,origin ):
 	#Stuff to store the number and origin
-	send_text.text(number, 'Now the destination please?');
+	send_text.text(number, 'Now the destination please?')
 	return
