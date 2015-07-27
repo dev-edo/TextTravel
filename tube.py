ee95863e047b9b1f1db97fc427bc566f868194bd
Mike Curtis <bzrk262.mktech@gmail.com>
2015-07-28 00:57:00 +0100

    Update tube.py
    'line' can now handle capitalsation (e.g. NORTHERN + northern will both work)

--- tube.py
+++ tube.py
@@ -8,7 +8,7 @@ response = urllib2.urlopen('https://api.tfl.gov.uk/line/mode/tube/status')
 data = json.load(response)   
 print 
 
-line.lower()
+line = line.lower()
 
 for x in data:
 	if x["id"]==line:
