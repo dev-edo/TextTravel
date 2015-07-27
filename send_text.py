992f46159a4cd31976509e68b8f609576f216cc7
Hugh Wells <wellshb@deanclose.org.uk>
2015-07-27 13:38:23 +0100

    Text API fucn

---
+++ send_text.py
@@ -0,0 +1,20 @@
+
+def text( number,message ): 
+	from clockwork import clockwork
+ 
+	api = clockwork.API('bc7209eb2eb0c99f74d7a96c03043f5271284866')
+ 
+	message = clockwork.SMS(
+	    to = number, 
+	    message = message)
+ 
+	response = api.send(message)
+ 
+	if response.success:
+    		print (response.id)
+	else:
+	    print (response.error_code)
+	    print (response.error_description)
+	return
+
+text('07837283251', 'Hey, this is a great and useful text');
