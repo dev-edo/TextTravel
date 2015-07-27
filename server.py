ae65c8c3d8e4b3507ba6ad6b8f84ac86be598a42
Edo Spigel-Emmerich <developer.edo@gmail.com>
2015-07-27 13:55:30 +0100

    server stage 1 finished

---
+++ server.py
@@ -0,0 +1,25 @@
+from wsgiref.simple_server import make_server
+from cgi import parse_qs, escape
+
+def texttravel(environ, start_response):
+    response_body = "Hello, World!"
+
+    status = '200 OK'
+    response_headers = [('Content-Type', 'text/plain'),
+                        ('Content-Length', str(len(response_body)))]
+    start_response(status, response_headers)
+
+    d = parse_qs(environ['QUERY_STRING'])
+    content = d.get('content', [''])[0] 
+    from_sender = d.get('from', [''])[0]
+    print content
+    print from_sender
+    return [response_body]
+httpd = make_server(
+   'localhost', # The host name.
+   8051, # A port number where to wait for the request.
+   texttravel # Our application object name, in this case a function.
+   )
+
+# Wait for a single request, serve it and quit.
+httpd.handle_request()
