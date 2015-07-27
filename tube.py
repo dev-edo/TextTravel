1c642fc4330b2498d609956e46bd9a6669f4a192
Hugh Wells <wellshb@deanclose.org.uk>
2015-07-27 14:58:31 +0100

    Tube status

--- tube.py
+++ tube.py
@@ -1,3 +1,9 @@
-import send_text.py
+import send_text
+import urllib2
+import json
 
 line = raw_input('Enter Tube line:')
+
+response = urllib2.urlopen('https://api.tfl.gov.uk/line/mode/tube/status')
+data = json.load(response)   
+print data
