61cdb815bc809188cffd143961a41953cababdf2
Mike Curtis <bzrk262.mktech@gmail.com>
2015-07-28 14:54:03 +0100

    Update bus.py

--- bus.py
+++ bus.py
@@ -4,6 +4,7 @@ import json
 
 def bus(number, message):
 	
+	switch = False
 	route = message[1]
 
 	response = urllib2.urlopen('https://api.tfl.gov.uk/line/mode/bus/status')
@@ -14,8 +15,15 @@ def bus(number, message):
 
 	for x in data:
 		if x["id"]==route:
+			switch = True
 			print x["name"]
 			print x["lineStatuses"][0]["statusSeverityDescription"]
 
 			send = x["name"] + ": " + x["lineStatuses"][0]["statusSeverityDescription"]
-			send_text.text(number, send)
+			
+	if switch is True:
+		send_text.text(number, send)
+		
+	if switch is False:
+		print "Whoops! Error made."
+		send_text.text(number, "Not a real route No.!")
