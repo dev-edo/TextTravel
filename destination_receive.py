99f63a9c54741b09c580f68936b3ee0536d3c8e2
Edo Spigel-Emmerich <developer.edo@gmail.com>
2015-07-28 09:56:09 +0100

    fixed

--- destination_receive.py
+++ destination_receive.py
@@ -1,5 +1,6 @@
-import send_text
-def destination( number,origin ):
+import send_text, sessions
+def destination(number, destination):
 	#Stuff to store the number and origin
 	send_text.text(number, 'Hang in there!')
+	sessions.add_destination(nnumber, destination)
 	return
--- help.py
+++ help.py
@@ -2,8 +2,8 @@ import send_text
 import sessions
 
 
-def help( number ): 
+def help(number): 
 	message = 'Text me your origin to get started'
-	send_text.text(number,message)
+	send_text.text(number, message)
 	sessions.insert(number)
 	return
