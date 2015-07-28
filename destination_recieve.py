8a05d2fb3fd2e3245539539f39d5eb60b7314c76
Edo Spigel-Emmerich <developer.edo@gmail.com>
2015-07-28 10:23:13 +0100

    fixed spelling issue

--- destination_recieve.py
+++ destination_recieve.py
@@ -1,5 +1,5 @@
 import send_text, sessions
-def destination_receive(number, destination):
+def destination(number, destination):
 	#Stuff to store the number and origin
 	send_text.text(number, 'Hang in there!')
 	sessions.add_destination(nnumber, destination)
