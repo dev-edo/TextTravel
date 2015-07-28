dfe6eed96ee26ae232891a2a8b19548413eef0e9
Mike Curtis <bzrk262.mktech@gmail.com>
2015-07-28 15:17:25 +0100

    Update tube.py

--- tube.py
+++ tube.py
@@ -5,25 +5,24 @@ import json
 def tube(number, message):
 	
 	switch = False
+	
 	line = message[1]
-
-	response = urllib2.urlopen('https://api.tfl.gov.uk/line/mode/tube/status')
-	data = json.load(response)   
-	print 
-
 	line = line.lower()
-
+	
+	response = urllib2.urlopen('https://api.tfl.gov.uk/line/mode/tube/status')
+	data = json.load(response)
+	
 	for x in data:
-		if x["name"]==line:
+		if x["id"] == line:
 			switch = True
 			print x["name"]
 			print x["lineStatuses"][0]["statusSeverityDescription"]
-
-			send = x["name"] + " Line Status: " + x["lineStatuses"][0]["statusSeverityDescription"]
+			
+	send = line.title() + " Line: " + x["lineStatuses"][0]["statusSeverityDescription"]
 	
 	if switch is True:
 		send_text.text(number, send)
 		
 	if switch is False:
 		print "Whoops! Error made."
-		send_text.text(number, "Failed!")
+		send_text.text(number, "Not a real line!")
