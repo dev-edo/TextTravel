6205ffd1a45311bf3dfe8135281de9ff21e92071
Edo Spigel-Emmerich <developer.edo@gmail.com>
2015-07-28 10:15:24 +0100

    hopefully fixed it

--- destination_receive.py
+++ destination_receive.py
@@ -1,5 +1,5 @@
 import send_text, sessions
-def destination(number, destination):
+def destination_receive(number, destination):
 	#Stuff to store the number and origin
 	send_text.text(number, 'Hang in there!')
 	sessions.add_destination(nnumber, destination)
