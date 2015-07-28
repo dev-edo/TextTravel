d12a9dd58ed7549d831bd12dd65320d2e16579f6
Mike Curtis <bzrk262.mktech@gmail.com>
2015-07-28 11:47:22 +0100

    Create bus.py

---
+++ bus.py
@@ -0,0 +1,21 @@
+import send_text
+import urllib2
+import json
+
+def tube(number, message):
+	
+	line = message[1]
+
+	response = urllib2.urlopen('https://api.tfl.gov.uk/line/mode/bus/status')
+	data = json.load(response)   
+	print 
+
+	line = line.lower()
+
+	for x in data:
+		if x["id"]==line:
+			print x["name"]
+			print x["lineStatuses"][0]["statusSeverityDescription"]
+
+			send = x["name"] + ": " + x["lineStatuses"][0]["statusSeverityDescription"]
+			send_text.text(number, send)
