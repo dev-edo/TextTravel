6c9a7a7f09b94535dbd4d583aa3c481b9d37717a
Hugh Wells <wellshb@deanclose.org.uk>
2015-07-28 11:53:28 +0100

    stuff

--- help.py
+++ help.py
@@ -1,9 +1,15 @@
 import send_text
 import sessions
 
+db_data = sessions.retrive_data(number)
+
 
 def help(number): 
 	message = 'Text me your origin to get started'
 	send_text.text(number, message)
-	sessions.insert(number)
+	if number == db_data[0]:
+		pass
+	else
+		sessions.insert(number)
+
 	return
