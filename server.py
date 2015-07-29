09bb25fc412d9d29bfedfdd10f2faf3e1d14b02c
Hugh Wells <wellshb@deanclose.org.uk>
2015-07-29 12:41:45 +0100

    added bus to main fucntions

--- server.py
+++ server.py
@@ -51,7 +51,7 @@ def texttravel(environ, start_response):
         return response_body
     if operator == 'bus':
         item = spl_txt[1]
-        bus.bus(number, spl_txt[1])
+        bus.bus(number, item)
         return response_body
     else:
       inference.infer(number,content,spl_txt,db_data)
