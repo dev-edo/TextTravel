632bc5b475535dc17aad6b7728d3b3ecda77bce8
Mike Curtis <bzrk262.mktech@gmail.com>
2015-07-29 10:56:00 +0100

    Update tube.py

--- tube.py
+++ tube.py
@@ -8,14 +8,14 @@ def tube(number, message):
     data = json.load(response)
 
     switch = False
-    line = message
-    
     
     if (line.lower() == "hammersmith and city") or (line.lower() == "hammersmith") or (line.lower() == "hammersmith & city"):
         line = "hammersmith-city"
 
     if (line.lower() == "waterloo and city") or (line.lower() == "waterloo") or (line.lower() == "waterloo & city"):
         line = "waterloo-city"
+    
+    line = message
 
     for x in data:
         if x["id"] == line.lower():
