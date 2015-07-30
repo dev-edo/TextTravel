eed2b4d904304204762ab87c057237fbf8b1568e
Edo Spigel-Emmerich <developer.edo@gmail.com>
2015-07-30 20:06:02 +0100

    added help operator, hugh should add his new function to the explination..., before the alst text.

--- interface.py
+++ interface.py
@@ -36,7 +36,15 @@ def process(mobile, message):
     elif operator == "help":
         send_text.text(mobile, "Text tube {line} for tube status, bus {number} for bus status and To {dest} then From {org} for route info")
 
-
+    #help operator:
+    elif operator == "help":
+        send_text.text(mobile, "Hi! Thanks for using traxt. Our service is simple to use:")
+        send_text.text(mobile, "1) If you want directions, send a text containing either the start or end point, formatted like so: ")
+        send_text.text(mobile, "To set the destination, send a message starting with \"to\" directly followed by the destination. e.g: \"to London\". You will then be prompted to send your start point, which should be formatted like so: ")
+        send_text.text(mobile, "To set the start point of your journey, send a message starting with \"from\", directly followed by the start point. e.g: \"from London\". You will then be prompted to send your destination, which should be formatted as shown above.")
+        send_text.text(mobile, "2) If you would like tube or bus status, text \"tube [underground line]\" or \"bus [bus No.]\" ")
+        send_text.text(mobile, "By sending \"reset\", your session will be cleared, although it will also be cleared automatically. Thanks for using traxt.")
+        
 
     # so we don't know the operator.
     else:
