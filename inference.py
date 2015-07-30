b5412fa85e0db1fa40ba003b152b4966793ed971
Edo Spigel-Emmerich <developer.edo@gmail.com>
2015-07-30 09:33:52 +0100

    1234

--- inference.py
+++ inference.py
@@ -9,21 +9,28 @@ import bus
 def infer(number,message,spl_txt,db_data):
     print "AM I RUNNING JACOB?!??!  "
     if db_data != None:
+        print "1"
         db_phone = db_data[0]
         db_origin = db_data[1]
         db_destination = db_data[2]
+        print "2"
     else: 
+        print "3"
         sessions.insert(number)
         db_data = sessions.retrive_data(number)
         db_phone = db_data[0]
         db_origin = db_data[1]
         db_destination = db_data[2]
+        print "4"
 
         number = long(number)
+        print "5"
         db_phone = long(db_phone)
+        print "6"
         sep = " "
+        print "7"
         new_msg = sep.join(spl_txt)
-
+        print "8"
         operator = spl_txt[0]
         print operator
         print "-- operator"
@@ -33,46 +40,62 @@ def infer(number,message,spl_txt,db_data):
         print "-- origin on db"
         print db_destination    
         print "-- dest on db"
-
+        print "9"
 
         if operator == 'tube':
+            print "10"
             item = spl_txt[1]
             tube.tube(number, spl_txt[1])
+            print "11"
             return response_body
 
         elif operator == 'help':
+            print "12"
             send_text.text(number,'To get tube status, "tube {line}", for route info, "to {place}" and "from {place}"')
             return response_body
         elif operator == 'bus':
+            print "13"
             item = spl_txt[1]
             bus.bus(number, item)
             return response_body
         else:
-          route(number,content,spl_txt,db_data)
+            print "14"
+            route(number,content,spl_txt,db_data)
+            print "15"
         #******************************************************
         #Do NOT remove the commenting!
         #******************************************************
 
         #check to see what operator we are using as defined in 'operator'
         def route(number,message,spl_txt,db_data):
+            print "16"
             if operator == None:
                 #now check to see what existing fields have been written to
+                print "17"
                 if (db_origin != None) and (db_destination == None):
+                    print "a"
                     #if we have origin but no destination
                     #take the dest that has been sent and add to db, then google
+                    print "b"
                     inf_funcs.add_dest_google(number,message)
+                    print "c"
                     return
                 elif (db_origin == None) and (db_destination != None): 
+                    print "d"
                     #if there is nothing in origin and we have a dest for this number
                     #take the origin that has been sent and add to db, then google
                     inf_funcs.add_org_google(number,message)
+                    print "e"
                     return
 
                 elif (db_origin == None) and (db_destination == None): #neither have been populated, we assume starting with origin
                     #take the origin, explain decision and ask for destination
+                    print "f"
                     inf_funcs.inf_org(number,message)
+                    print "g"
                     return
             elif operator == 'to':
+                print "18"
                 #we have a destination 
                 #write to the database
                 sessions.add_destination(number, new_msg)
@@ -87,6 +110,7 @@ def infer(number,message,spl_txt,db_data):
                     return
 
             elif operator == 'from':
+                print "19"
                 #we have an origin 
                 #write it to the db
                 sessions.add_origin(number, new_msg)
@@ -97,9 +121,11 @@ def infer(number,message,spl_txt,db_data):
                     inf_funcs.origin_plz
                     return
             elif (operator == 'return') and (db_origin != None) and (db_destination != None):
+                print "20"
                 #swap stored values around, and run google function
                 inf_funcs.google_it(number, db_destination, db_origin)
                 return
             else:
+                print "21"
                 inf_funcs.error(number)
                 return
--- server.py
+++ server.py
@@ -9,51 +9,52 @@ import inference
 import bus
 def texttravel(environ, start_response):
     response_body = ""
-    print "1"
+    #print "1"
     status = '200 OK'
     response_headers = [('Content-Type', 'text/plain'),
                         ('Content-Length', str(len(response_body)))]
     start_response(status, response_headers)
-    print "2"
+    #print "2"
     d = parse_qs(environ['QUERY_STRING'])
     content = d.get('content', [''])[0] 
     from_sender = d.get('from', [''])[0]
     number = from_sender
-    print "3"
+    print content, number
+    #print "3"
 
 
     content = content.lower()
     spl_txt = content.split()
     db_data = sessions.retrive_data(number)
     if db_data != None:
-        print "4"
+        #print "4"
         db_phone = db_data[0]
         db_origin = db_data[1]
         db_destination = db_data[2]
-        print "5"
+        #print "5"
     else: 
-        print "6"
+        #print "6"
         sessions.insert(number)
         db_data = sessions.retrive_data(number)
         db_phone = db_data[0]
         db_origin = db_data[1]
         db_destination = db_data[2]
-        print "7"
+        #print "7"
     
     operator = spl_txt[0]
-    print "8"
+    #print "8"
     inference.infer(number,content,spl_txt,db_data)
-    print "9"
+    #print "9"
     return response_body
 
 
-print "a"
+#print "a"
 httpd = make_server(
    'localhost', # The host name.
    8051, # A port number where to wait for the request.
    texttravel # Our application object name, in this case a function.
    )
-print "b"
+#print "b"
 # Wait for a single request, serve it and quit.
 httpd.serve_forever()
-print "c"
+#print "c"
