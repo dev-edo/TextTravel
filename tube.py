105da8f935f3117cd5d03a5562a96d478ad64a69
Mike Curtis <bzrk262.mktech@gmail.com>
2015-07-31 09:33:22 +0100

    Fix tube.py

--- tube.py
+++ tube.py
@@ -5,11 +5,12 @@ import time
 
 
 def tube(number, message):
-
-    response = urllib2.urlopen('https://api.tfl.gov.uk/line/mode/tube/status')
-    data = json.load(response)
-
+    
     switch = False
+
+    response = urllib2.urlopen('https://api.tfl.gov.uk/line/mode/bus/status')
+    data = json.load(response)
+    print "Possible route no. sent: " + message
     
     if (message.lower() == "hammersmith and city") or (message.lower() == "hammersmith") or (message.lower() == "hammersmith & city"):
         message = "hammersmith-city"
@@ -19,21 +20,24 @@ def tube(number, message):
 
     for x in data:
         if x["id"] == message.lower():
+            message.lower()
             switch = True
             print x["name"]
             print x["lineStatuses"][0]["statusSeverityDescription"]
-        send = x["name"].title() + " Line: " + x["lineStatuses"][0]["statusSeverityDescription"]
-        if "reason" in x["lineStatuses"][0]:
-            print "Reason sent"
-            send2 = "Issue: " + x["lineStatuses"][0]["reason"]
-        send = message.title() + " Route: " + x["lineStatuses"][0]["statusSeverityDescription"]
-
-        if switch is True:
-            send_text.text(number, send)
-            
             if "reason" in x["lineStatuses"][0]:
-                time.sleep(3)
-                send_text.text(number, send2)
+                switch = True
+                print "Reason sent"
+                send2 = "Issue: " + x["lineStatuses"][0]["reason"]
+        send = message.title() + " Line: " + x["lineStatuses"][0]["statusSeverityDescription"]
 
-        if switch is False:
-            send_text.text(number, 'Not a real line!')
+
+    if switch is True:
+        send_text.text(number, send)
+        if "reason" in x["lineStatuses"][0]:
+            time.sleep(3)
+            send_text.text(number, send2)
+
+    if switch is False:
+        print "Error."
+        send_text.text(number, "Not a real route!")
+        api.send(message)
